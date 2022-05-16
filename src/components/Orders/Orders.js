a6bf97808197bb6fd7431b946d13e79dd38e4635
Alamin-git <alamin2.mas@gmail.com>
2022-05-17 02:57:51 +0600

    mongoDB data add

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -7,16 +7,16 @@ import useProducts from "../../hooks/useProducts";
 import { removeFromDb } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import ReviewItem from "../ReviewItem/ReviewItem";
-import './Orders.css'
+import './Orders.css';
 
 const Orders = () => {
    const [products, setProducts] = useProducts();
    const [cart, setCart] = useCart(products);
 
    const handelRemoveProduct = (product) => {
-      const rest = cart.filter(pd => pd.id !== product.id);
+      const rest = cart.filter(pd => pd._id !== product._id);
       setCart(rest);
-      removeFromDb(product.id)
+      removeFromDb(product._id)
    }
 
    return (
@@ -24,7 +24,7 @@ const Orders = () => {
          <div className="product-container">
             {
                cart.map(product => <ReviewItem
-                  key={product.id}
+                  key={product._id}
                   product={product}
                   handelRemoveProduct={handelRemoveProduct}
                ></ReviewItem>)
