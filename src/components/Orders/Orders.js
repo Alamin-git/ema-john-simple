964df659c0c1fbb3f5ecbe49e8122c9e32f94d57
Alamin-git <alamin2.mas@gmail.com>
2022-05-18 18:38:47 +0600

    cart page data lode by $in

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -11,7 +11,7 @@ import './Orders.css';
 
 const Orders = () => {
    const [products, setProducts] = useProducts();
-   const [cart, setCart] = useCart(products);
+   const [cart, setCart] = useCart();
 
    const handelRemoveProduct = (product) => {
       const rest = cart.filter(pd => pd._id !== product._id);
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,29 +2,44 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import useProducts from "../../hooks/useProducts";
-import { addToDb, getStoredCart } from "../../utilities/fakedb";
+import useCart from "../../hooks/useCart";
+import { addToDb } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
-const Shop = () => {
-   const [products, setProducts] = useProducts();
 
-   const [cart, setCart] = useState([]);
+
+
+const Shop = () => {
+   const [products, setProducts] = useState([]);
+   const [cart, setCart] = useCart();
+   const [pageCount, setPageCount] = useState(0);
+   const [page, setPage] = useState(0);
+   const [size, setSize] = useState(10);
+
 
    useEffect(() => {
-      const storedCart = getStoredCart();
-      const saveCart = [];
-      for (const id in storedCart) {
-         const addedProduct = products.find((product) => product._id === id);
-         if (addedProduct) {
-            const quantity = storedCart[id];
-            addedProduct.quantity = quantity;
-            saveCart.push(addedProduct);
-         }
-      }
-      setCart(saveCart);
-   }, [products]);
+      // page= কত নাম্বার পেজে আছে সেটা দেয়া । size= কত গুলো data লোড হবে সেটার জন্য। এইটা key=value  হিসাবে কাজ করে । 
+      
+      const url =`http://localhost:5000/product?page=${page}&size=${size}`;
+      fetch(url)
+         .then((res) => res.json())
+         .then((data) => setProducts(data));
+
+      // যেহেতু কত number page আছি এবং কইটা data lode হবে  তার ওপর নির্ভর করে data lode করবে । তাই ডিপেন্ডেন্সি দেয়া হয়েছে।
+   }, [page, size]);
+
+
+
+   useEffect(()=>{
+      fetch('http://localhost:5000/productcount')
+      .then(res => res.json())
+      .then(data => {
+         const count = data.count;
+         const pages = Math.ceil(count/10);
+         setPageCount(pages);
+      })
+   },[]);
 
    const handelAddToAddCart = (selectedProduct) => {
       // console.log(product);
@@ -49,11 +64,28 @@ const Shop = () => {
          <div className="shop-container">
             {products.map((product) => (
                <Product
-                  key={product._id}
+                  key={product._id} 
                   product={product}
                   handelAddToAddCart={handelAddToAddCart}
                ></Product>
             ))}
+            <div className="pagination">
+               {
+                  // 1 line ও করা যেত , সুবিধার জন্য 2 line করা হইছে ;
+                  [...Array(pageCount).keys()]
+                  .map(number => <button 
+                     className={page === number ? 'selected' : ''}
+                     onClick={() => {setPage(number)}}
+                  >{number + 1}</button>)
+               }
+               {size}
+               <select onClick={e => setSize(e.target.value)}>
+                  <option value="5">5</option>
+                  <option value="10" selected>10</option>
+                  <option value="15">15</option>
+                  <option value="20">20</option>
+               </select>
+            </div>
          </div>
          <div className="cart-container">
             <Cart cart={cart}>
